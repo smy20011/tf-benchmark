cc8d4bdddab6a355ce8ae0592b8762abaacd076a
Siyuan Ma <smy20011@gmail.com>
2018-09-24 20:54:57 -0700

    Create Docker File

---
+++ benchmark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### Change this to match the repo root dir ###
+RPATH="/benchmarks"
+
+#Ouput results to home dir
+RESULTS="$HOME/tf_cnn_bench_results.txt"
+#Full path
+FPATH="$RPATH/scripts/tf_cnn_benchmarks"
+#Run the benches
+python "$FPATH/tf_cnn_benchmarks.py" --num_gpus=1 --batch_size=64 --variable_update=parameter_server --optimizer=sgd --model=resnet50 --local_parameter_device=gpu | tee -a "$RESULTS"
+sleep 5
+python "$FPATH/tf_cnn_benchmarks.py" --num_gpus=1 --batch_size=32 --variable_update=parameter_server --optimizer=sgd --model=resnet152 --local_parameter_device=gpu | tee -a "$RESULTS"
+sleep 5
+python "$FPATH/tf_cnn_benchmarks.py" --num_gpus=1 --batch_size=512 --variable_update=parameter_server --optimizer=sgd --model=alexnet --local_parameter_device=gpu | tee -a "$RESULTS"
+sleep 5
+python "$FPATH/tf_cnn_benchmarks.py" --num_gpus=1 --batch_size=32 --variable_update=parameter_server --optimizer=sgd --model=vgg16 --local_parameter_device=gpu | tee -a "$RESULTS"
+sleep 5
+python "$FPATH/tf_cnn_benchmarks.py" --num_gpus=1 --batch_size=64 --variable_update=parameter_server --optimizer=sgd --model=inception3 --local_parameter_device=gpu | tee -a "$RESULTS"
